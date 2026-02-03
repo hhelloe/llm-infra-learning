52445ccf9cab79fd349f9ede77621c96d530632f
hhelloe <2421367876@qq.com>
2026-02-03 17:33:44 +0800

    rename

---
+++ 3.mmap_vs_read/create_test_file.py
@@ -0,0 +1,11 @@
+# create_test_file.py
+import os
+
+FILE = "test_100mb.bin"
+SIZE = 100 * 1024 * 1024  # 100MB
+
+if not os.path.exists(FILE):
+    with open(FILE, "wb") as f:
+        f.write(os.urandom(SIZE))
+
+print("file ready:", FILE)
